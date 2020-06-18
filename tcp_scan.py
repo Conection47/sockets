30de805f30fa3e91b55f4a7ae31d8f6a75b45fec
Kaiky Pereira Almeida <64940807+Conection47@users.noreply.github.com>
2020-06-18 16:52:10 +0000

    Update tcp_scan.py
    Without bugs and comments added.

--- tcp_scan.py
+++ tcp_scan.py
@@ -1,44 +1,62 @@
+#Import modules
 import socket
 import pyfiglet
 import sys
 
+#Define variables
 open_ports = []
+exist_ports_open = 0
 comands = {"-h", "--help"}
 args = sys.argv
 
-
+#TCP SCANNER banner / authory of </Conection>
 main = pyfiglet.figlet_format("TCP SCAN")
 print(main, "Developed by: </Conection>")
 
+#Check if arguments/values are correct
 try:
 
     ip = str(args[1])
     start_port = int(args[2])
     end_port = int(args[3])
-    
+
+#Else...
 except:
+
+    if len(args) == 1:
+        print("\n[ERROR] You put wrong arguments or missing arguments, \"--help\" to help.")
+        sys.exit(0)
+
     if len(args) == 2:
         if args[1] not in comands:
             print("\n[ERROR] You put wrong arguments or missing arguments, \"--help\" to help.")
             sys.exit(0)
 
+#If user type "-h" or "--help" help him ;)
 if args[1] == "-h" or args[1] == "--help":
     print("\nUsage: tcp_scan.py <ip> <starting_port> <ending_port>")
     sys.exit(0)
 
-
+#Define type of conection (IP/TCP) and set the max of time to try
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.settimeout(0.6)
+client.settimeout(0.05)
 
+#List of ports to scan
 for portas in range(int(start_port), int(end_port)):
-    try:
-        result = client.connect((ip, portas))
-        if result == 0:
-            open_ports.append(portas)
-    except:
+
+    #Try to connect
+    result = client.connect_ex((ip, portas))
+
+    print(result)
+    #If port is open:
+    if result == 0:
+        print(f"Port {portas} is open! ")
+        exist_ports_open = 1
+    #Else:
+    else:
         pass
-
-for show_open_ports in open_ports:
-    print(f"The port {show_open_ports} is open! ")
-
 client.close()
+
+#If he do not find any open port:
+if exist_ports_open == 0:
+    print("No ports open! ")
